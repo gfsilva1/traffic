50b6fa9d8a6f7c484e4a1d87d43561c615320fca
Ricardo Hasenack <rhasenack@gmail.com>
2021-08-31 12:56:57 -0300

    added form to first page

--- app/controllers/viagems_controller.rb
+++ app/controllers/viagems_controller.rb
@@ -1,14 +1,20 @@
 class ViagemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @viagens = Viagem.all.where("user_id = #{current_user.id}")
+    @viagem = Viagem.new
   end
 
-  def new
-  end
+  # def new
+  #   create
+  # end
 
   def create
-
+    @viagem = Viagem.new(viagem_params)
+    @viagem.user_id = current_user.id
+    @viagem.save!
+    redirect_to viagem_path(@viagem)
   end
 
   def edit
@@ -17,5 +23,20 @@ class ViagemsController < ApplicationController
 
   def update
 
+  end
+
+  def show
+    @viagem = Viagem.find(params[:id])
+  end
+
+  def maps_api
+    @params = viagem_params
+
+  end
+
+  private
+
+  def viagem_params
+    params.require(:viagem).permit(:origem, :destino)
   end
 end
