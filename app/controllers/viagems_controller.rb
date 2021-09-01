169c144a04eb06149bb79174abf1df1981a998c8
Ricardo Hasenack <rhasenack@gmail.com>
2021-09-01 10:55:22 -0300

    fix google maps gem

--- app/controllers/viagems_controller.rb
+++ app/controllers/viagems_controller.rb
@@ -37,44 +37,45 @@ class ViagemsController < ApplicationController
   end
 
   def maps_api
-    @origem = Google::Maps.geocode("Sao Paulo").first
-    @destino = Google::Maps.geocode("Ubatuba").first
-    route = Google::Maps.route("#{@origem.latitude}, #{@origem.longitude}", "#{@destino.latitude}, #{@destino.longitude}")
-    steps = []
-    route.steps.each do |step|
-      steps << step if step.distance.value > 10000
-    end
 
-    #ESTRADA
-    url = URI("https://roads.googleapis.com/v1/nearestRoads?points=-23.3862104,-46.15781260000001&key=#{ENV['MAPS_API_KEY']}")
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-    request = Net::HTTP::Get.new(url)
-    response = https.request(request)
+    # @origem = Google::Maps.geocode("Sao Paulo").first
+    # @destino = Google::Maps.geocode("Ubatuba").first
+    # route = Google::Maps.route("#{@origem.latitude}, #{@origem.longitude}", "#{@destino.latitude}, #{@destino.longitude}")
+    # steps = []
+    # route.steps.each do |step|
+    #   steps << step if step.distance.value > 10000
+    # end
+
+    # #ESTRADA
+    # url = URI("https://roads.googleapis.com/v1/nearestRoads?points=-23.3862104,-46.15781260000001&key=#{ENV['MAPS_API_KEY']}")
+    # https = Net::HTTP.new(url.host, url.port)
+    # https.use_ssl = true
+    # request = Net::HTTP::Get.new(url)
+    # response = https.request(request)
 
 
-    # REVERSE GEOCODE
+    # # REVERSE GEOCODE
 
-    reverse_geocode_responses = []
-    reverse_geocode_routes = []
-    steps.each do |step|
-      url = URI("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{step.end_location.lat},#{step.end_location.lng}&key=#{ENV['MAPS_API_KEY']}")
-      https = Net::HTTP.new(url.host, url.port)
-      https.use_ssl = true
-      request = Net::HTTP::Get.new(url)
-      response = https.request(request)
-      json_response = JSON.parse(response.body)
-      reverse_geocode_responses << json_response
+    # reverse_geocode_responses = []
+    # reverse_geocode_routes = []
+    # steps.each do |step|
+    #   url = URI("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{step.end_location.lat},#{step.end_location.lng}&key=#{ENV['MAPS_API_KEY']}")
+    #   https = Net::HTTP.new(url.host, url.port)
+    #   https.use_ssl = true
+    #   request = Net::HTTP::Get.new(url)
+    #   response = https.request(request)
+    #   json_response = JSON.parse(response.body)
+    #   reverse_geocode_responses << json_response
 
-      reverse_geocode_responses.each do |response|
-        response["results"].each do |result|
-          result["address_components"].each do |component|
-            reverse_geocode_routes << component if component["types"].include? 'route'
-          end
-        end
-      end
-    end
-    raise
+    #   reverse_geocode_responses.each do |response|
+    #     response["results"].each do |result|
+    #       result["address_components"].each do |component|
+    #         reverse_geocode_routes << component if component["types"].include? 'route'
+    #       end
+    #     end
+    #   end
+    # end
+    # raise
 
   end
 
