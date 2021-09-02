8834325b92e6c319e5e70b0a7add3474ca357d90
Nathan Debaz <nathandebaz266@gmail.com>
2021-09-02 13:54:23 -0300

    autocomplete feito

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,30 +1,14 @@
-const autocomplete = () => {
-  const input = document.getElementById('autocomplete');
-  input.addEventListener('change', initAutocomplete());
-  let autocomplete;
-  function initAutocomplete() {
-    console.log('To entrando');
-    autocomplete = new google.maps.places.Autocomplete(
+const initAutocomplete = () => {
+  console.log('To entrando');
+  document.querySelectorAll('.autocomplete').forEach((input) => {
+    new google.maps.places.Autocomplete(
       input,
       {
-        types: ['establishment'],
-        componentRestrictions: { 'country': 'BR' },
-        fields: ['place_id', 'geometry', 'name', 'address_components']
-      });
-    autocomplete.addListener('place_changed', onPlaceChanged);
-  }
-
-  function onPlaceChanged() {
-    let place = autocomplete.getPlace();
-
-    if (!place.geometry) {
-      input.placeholder = 'Escreva uma origem';
-    } else {
-      document.getElementById('details').innerHTML = place.name;
-    }
-  }
-  console.log('to saindo');
+          types: ['address'],
+          componentRestrictions: { 'country': 'BR' },
+          fields: ['place_id', 'geometry', 'name', 'address_components']
+        });
+  })
 }
 
-
-export {autocomplete}
+export { initAutocomplete }
