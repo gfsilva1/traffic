c5591f110518a252fa1e35f8eee8bbef176637bb
Nathan Debaz <nathandebaz266@gmail.com>
2021-09-02 16:37:09 -0300

    coloquei regras globais de scss agr funcionando

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,14 +1,14 @@
-const initAutocomplete = () => {
-  console.log('To entrando');
-  document.querySelectorAll('.autocomplete').forEach((input) => {
-    new google.maps.places.Autocomplete(
-      input,
-      {
-          types: ['address'],
-          componentRestrictions: { 'country': 'BR' },
-          fields: ['place_id', 'geometry', 'name', 'address_components']
-        });
-  })
-}
+//const initAutocomplete = () => {
+//  console.log('To entrando');
+//  document.querySelectorAll('.autocomplete').forEach((input) => {
+//    new google.maps.places.Autocomplete(
+//      input,
+//      {
+//          types: ['address'],
+//          componentRestrictions: { 'country': 'BR' },
+//          fields: ['place_id', 'geometry', 'name', 'address_components']
+//        });
+//  })
+//}
 
-export { initAutocomplete }
+//export { initAutocomplete }
