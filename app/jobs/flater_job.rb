bb8ba1fb859a9323d4197ae6e3d0980defecb8d7
Nathan Debaz <nathandebaz266@gmail.com>
2021-09-09 11:13:51 -0300

    side com logica funcionando mas ta preso no enqueued

--- app/jobs/flater_job.rb
+++ app/jobs/flater_job.rb
@@ -3,7 +3,7 @@ class FlaterJob < ApplicationJob
 
   def perform(user_id)
     user = User.find(user_id)
-    user.trips.each do |trip|
+    user.trips.where('date >= ?', Date.today).each do |trip|
       hour = trip.time
       trip.origin_destination_routes.roads.each do |road|
         road.road_cars.each do |element|
@@ -18,7 +18,7 @@ class FlaterJob < ApplicationJob
           end
         end
       end
-      if (@real.number_of_cars > @aft.number_of_cars || @real.number_of_cars > @bef.number_of_cars)
+      if ((@aft && @real.number_of_cars > @aft.number_of_cars) || (@bef && @real.number_of_cars > @bef.number_of_cars))
         NewComment.with(message: "There is a faster schedule for your trip from #{trip.origin_destination_routes.origin.name} to #{trip.origin_destination_routes.destination.name}", link: "Check it out").deliver(user)
       end
     end
